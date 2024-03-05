e4848ae48e67c894eddea0c12118efdee2c57bec
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2024-03-05 09:42:21 -0300

    irei verificar porque o ENV parou de funcionar sem motivo aparente.

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,15 +1,16 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
-const dbName = process.env.DATABASE || ''; 
-const dbUser = process.env.DB_USER || '';
-const dbPassword = process.env.DB_PASSWORD;
+const dbName = 'beatu';
+const dbUser = process.env.DB_USER || 'root';
+const dbPassword = 'brunosenhas';
 const dbHost = process.env.DB_HOST;
 const dbPort = Number(process.env.DB_PORT);
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
     dialect: 'mysql',
     host: dbHost,
-    port: dbPort
+    port: dbPort,
+    sync: { force: true },
 });
 
 export default sequelize;
