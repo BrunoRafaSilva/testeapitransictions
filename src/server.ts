7118aae09c1bcba812ab36923f620a3b17d10b88
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2024-03-05 09:37:16 -0300

    identificado que nao estava sendo recebido um formato JSON, aplicado correção

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
 import express from 'express';
 import router from './routers';
 import database from './database/db';
-require('dotenv').config({ path: '{./config/.env}' })
-database.sync()
+import dotenv from 'dotenv';
+dotenv.config({ path: './config/.env' });
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
-// Usando o roteador dinamico definido 
-app.use(router)
+// Usando o roteador dinamico definido
+app.use(router);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port} .`)
-})
+app.listen(port, async () => {
+    try {
+        await database.sync();
+        console.log('Tabela .');
+    } catch (error) {
+        console.error('Erro ao sincronizar.', error);
+    }
+    console.log(`Server is running on port ${port} .`);
+});
