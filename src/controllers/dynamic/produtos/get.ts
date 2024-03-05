b63d402dcc44a7ce6b74b5c1516e127756361b70
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2024-03-05 14:51:59 -0300

    fix leitura

--- src/controllers/dynamic/produtos/get.ts
+++ src/controllers/dynamic/produtos/get.ts
@@ -2,6 +2,10 @@ import { Response, Request } from 'express';
 import Produtos from '../../../Models/Produto';
 
 export default async function findAll(req: Request, res: Response) {
-    const clientes = await Produtos.findAll({ limit: req.pagination?.limit, offset: req.pagination?.offset });
+    const clientes = await Produtos.findAll({
+        limit: req.pagination?.limit,
+        offset: req.pagination?.offset,
+    });
+
     res.json(clientes);
 }
