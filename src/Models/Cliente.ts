0e4ba2ae68418b073e7681fb8066dcdb41532ad8
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2024-03-05 17:58:41 -0300

    trabalhando na formalução de uma nova tabela

--- src/Models/Cliente.ts
+++ src/Models/Cliente.ts
@@ -1,4 +1,17 @@
 import Sequelize from 'sequelize';
 import database from '../database/db';
 
-const Cliente = database.define('Clientes', {});
+const Cliente = database.define('Cliente', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: true,
+        primaryKey: true,
+    }, nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }, contato: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+});
