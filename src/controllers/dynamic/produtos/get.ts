b6119e8bd236b68039e8d2e6d1698362f5c96a66
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2024-03-05 11:07:26 -0300

    rota get produtos

--- src/controllers/dynamic/produtos/get.ts
+++ src/controllers/dynamic/produtos/get.ts
@@ -2,6 +2,6 @@ import { Response, Request } from 'express';
 import Produtos from '../../../Models/Produto';
 
 export default async function findAll(req: Request, res: Response) {
-    const clientes = await Produtos.findAll({ ...req.query });
+    const clientes = await Produtos.findAll();
     res.json(clientes);
 }
