93b283d47ad419cff352fce634465a19c1d50bf5
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2024-03-05 15:56:03 -0300

    implementado get por id

---
+++ src/Models/Cliente.ts
@@ -0,0 +1,4 @@
+import Sequelize from 'sequelize';
+import database from '../database/db';
+
+const Cliente = database.define('Clientes', {});
--- src/controllers/dynamic/produtos/get.ts
+++ src/controllers/dynamic/produtos/get.ts
@@ -1,11 +1,21 @@
 import { Response, Request } from 'express';
+import { Op, WhereOptions } from 'sequelize';
 import Produtos from '../../../Models/Produto';
 
 export default async function findAll(req: Request, res: Response) {
+    const where: WhereOptions = {};
+    if (req.query.id) {
+        where.id = req.query.id;
+    }
+
     const clientes = await Produtos.findAll({
         limit: req.pagination?.limit,
         offset: req.pagination?.offset,
+        where: where,
     });
+    if (clientes.length === 0) {
+        return res.status(404).json({ message: 'Nenhum produto encontrado' });
+    }
 
     res.json(clientes);
 }
