dbce4ffbb099ee1022069ada5998dae4abc6daac
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2024-03-06 12:24:44 -0300

    arrow function

--- src/controllers/dynamic/produtos/post.ts
+++ src/controllers/dynamic/produtos/post.ts
@@ -1,22 +1,21 @@
 import z from 'zod';
 import { Response, Request } from 'express';
 import Produtos from '../../../Models/Produto';
+import zodValidator from '../../../middlewares/zodError';
 
 export default async (req: Request, res: Response) => {
     const produtoSchema = z.object({
         nome: z.string(),
-        preco: z.number(),
+        preco: z.string(),
         descricao: z.string().optional(),
     });
 
-    const newProduto = produtoSchema.safeParse(req.query);
+    const newProduto = produtoSchema.safeParse(req.body);
 
-    if (!newProduto.success) return res.status(500).json({ error: true, message: 'Erro ao criar produto' });
+    if (!newProduto.success) return zodValidator(newProduto.error, res);
 
-    const result = await Produtos.create(newProduto);
+    const result = await Produtos.create(newProduto.data);
 
-    if (!result) { return res.status(500).json({ error: true, message: 'Erro ao criar produto' }); }
-    else {
-        return res.status(201).json({ error: false, message: result });
-    }
+    if (!result) return res.status(500).json({ error: true, message: 'Erro ao criar produto' });
+    else return res.status(201).json({ error: false, message: result });
 };
