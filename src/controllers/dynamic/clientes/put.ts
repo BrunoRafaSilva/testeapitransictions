11aba20cd58d49c980afe19c098138a824a63b4d
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2024-03-06 12:48:56 -0300

    criado rota put para clientes

---
+++ src/controllers/dynamic/clientes/put.ts
@@ -0,0 +1,32 @@
+import z from 'zod';
+import { Request, Response } from 'express';
+import { WhereOptions } from 'sequelize';
+import Cliente from '../../../Models/Cliente';
+import ZodValidator from '../../../middlewares/zodError';
+
+export default async (req: Request, res: Response) => {
+    const where: WhereOptions = {};
+    if (req.query.id) {
+        where.id = req.query.id;
+    }
+
+    const clienteSchema = z.object({
+        nome: z.string().optional(),
+        contato: z.string().optional(),
+        observacao: z.string().optional(),
+    });
+
+    const updateCliente = clienteSchema.safeParse(req.body);
+
+    if (!updateCliente.success) return ZodValidator(updateCliente.error, res);
+
+    await Cliente.update(updateCliente.data, { where: where });
+
+    Cliente.findByPk(where.id).then((result) => {
+        if (result === null) {
+            res.status(404).json({ error: true, message: 'Cliente informado não encontrado' });
+        } else {
+            res.status(200).json({ error: false, data: result });
+        }
+    });
+};
